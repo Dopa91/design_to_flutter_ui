40ebe4c1b39e740e8a37f5c6a1ae33638f270df1
Dopa91 <code.dopatka@gmail.com>
2024-09-26 17:02:36 +0200

    inital commit <3

--- lib/hike_address.dart
+++ lib/hike_address.dart
@@ -1,22 +1,63 @@
 import 'package:design_to_flutter_ui/hike_name_and_address.dart';
 import 'package:flutter/material.dart';
 
-class HikeAddress extends StatelessWidget {
-  const HikeAddress({
-    super.key,
-  });
+class HikeAddress extends StatefulWidget {
+  const HikeAddress({super.key});
+
+  @override
+  State<StatefulWidget> createState() => HikeAddressState();
+}
+
+class HikeAddressState extends State<HikeAddress> {
+  int counter = 41;
+  IconData iconData = Icons.star;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        HikeNameAndAddress(),
-        Spacer(),
-        Icon(Icons.star, color: Colors.red),
-        SizedBox(width: 4),
-        Text("41"),
+        const HikeNameAndAddress(),
+        const Spacer(),
+        IconButton(
+          onPressed: () {
+            counter++;
+            setState(() {});
+          },
+          icon: const Icon(Icons.star),
+          color: Colors.red,
+        ),
+        //Icon(Icons.star, color: Colors.red),
+        const SizedBox(width: 4),
+        Text(counter.toString()),
       ],
     );
   }
 }
+
+// class HikeAddress extends StatelessWidget {
+//   const HikeAddress({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         const HikeNameAndAddress(),
+//         const Spacer(),
+//         IconButton(
+//           onPressed: () {
+//             print("Button gedrückt");
+//           },
+//           icon: const Icon(Icons.star),
+//           color: Colors.red,
+//         ),
+//         //Icon(Icons.star, color: Colors.red),
+//         const SizedBox(width: 4),
+//         const Text("41"),
+//       ],
+//     );
+//   }
+// }
